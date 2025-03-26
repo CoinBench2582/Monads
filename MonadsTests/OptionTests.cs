5bdeaea273db2d50de7371e476e4a896e29adfdf
CoinBench <coinbench.riot@outlook.com>
2025-03-26 20:34:56 +0100

    Better code style

--- MonadsTests/OptionTests.cs
+++ MonadsTests/OptionTests.cs
@@ -1,8 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-using Monads;
-
-using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+﻿using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
 namespace Monads.Tests
 {
