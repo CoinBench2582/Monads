e5cc9767da3a47d1654c0ded974881c66e94dc55
CoinBench <coinbench.riot@outlook.com>
2025-03-26 21:08:07 +0100

    Basic behaviour

--- Monads/Option.cs
+++ Monads/Option.cs
@@ -3,10 +3,11 @@
     public class Option<T>
         where T : class
     {
-        protected readonly T? _value;
+        protected T? _value;
 
         public T Value => _value ?? throw new InvalidOperationException();
 
+        //[System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
         [System.Diagnostics.CodeAnalysis.MemberNotNullWhen(true, nameof(_value))]
         public bool HasValue => _value is not null;
 
@@ -15,8 +16,20 @@
         public static Option<T> Some(T value) => new(value);
 
         public static Option<T> None() => new(null);
+
+        public Option<R> Bind<R>(Func<T, R> func) where R : class
+            => HasValue ? new(func(_value)) : new();
+
+        public void Resolve(Action<T> some, Action none)
+        {
+            if (HasValue)
+                some(_value!);
+            else
+                none();
     }
 
+        public R Resolve<R>(Func<T, R> some, Func<R> none)
+            => HasValue ? some(_value) : none();
     public class Some<T> : Option<T>
         where T : class
     {
