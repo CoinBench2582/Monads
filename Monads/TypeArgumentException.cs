31df1ea306e6654364e479f6be155e671c1836eb
CoinBench2582 <coinbench.riot@outlook.com>
2025-05-07 13:15:14 +0200

    add TypeArgumentException for implementation of IOption non-constranted methods

---
+++ Monads/TypeArgumentException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monads
+{
+    [Serializable]
+    /// <inheritdoc cref="ArgumentException"/>
+    public sealed class TypeArgumentException : ArgumentException
+    {
+        /// <inheritdoc cref="ArgumentException()"/>
+        public TypeArgumentException() { }
+        /// <inheritdoc cref="ArgumentException(string)"/>
+        public TypeArgumentException(string message) : base(message) { }
+        /// <inheritdoc cref="ArgumentException(string, Exception)"/>
+        public TypeArgumentException(string message, Exception inner) : base(message, inner) { }
+        /// <inheritdoc cref="ArgumentException(string, string)"/>
+        public TypeArgumentException(string message, string paramName) : base(message, paramName) { }
+        /// <inheritdoc cref="ArgumentException(string, string, Exception)"/>
+        public TypeArgumentException(string message, string paramName, Exception inner) : base(message, paramName, inner) { }
+    }
+}
